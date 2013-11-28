5a8864814fc2048616510fbf9ffcba84fb6aa6d5
mrjusepelo <jperezlondono@gmail.com>
2013-11-28 09:57:57 -0500

    deploy2 pdf

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,12 +1,12 @@
 PDFKit.configure do |config|
  
-  # if ["development"].include?(Rails.env)
+  if ["development"].include?(Rails.env)
     #only if your are working on 32bit machine
     config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-i386').to_s
-  # else
-    #if your site is hosted on heroku or any other hosting server which is 64bit
-    # config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
-  # end
+  else
+    # if your site is hosted on heroku or any other hosting server which is 64bit
+    config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
+  end
  
   config.default_options = {
     :encoding=>"UTF-8",
