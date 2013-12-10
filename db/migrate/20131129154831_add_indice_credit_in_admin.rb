44d2ba23914835f54c3abe836224c7cc1a88fa66
mrjusepelo <jperezlondono@gmail.com>
2013-12-10 18:16:47 -0500

    archivo de migracion

--- db/migrate/20131129154831_add_indice_credit_in_admin.rb
+++
@@ -1,6 +0,0 @@
-class AddIndiceCreditInAdmin < ActiveRecord::Migration
-  def change
-    add_index :credits, :admin_id
-
-  end
-end
--- db/migrate/20131210204420_add_admin_creator_values_to_credits.rb
+++
@@ -1,5 +0,0 @@
-class AddAdminCreatorValuesToCredits < ActiveRecord::Migration
-  def change
-    add_reference :credits, :admin_creator, index: true
-  end
-end
