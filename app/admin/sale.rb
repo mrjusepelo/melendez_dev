f8046f5753dce0b3c98e6f4ff2618d4582ae6274
mrjusepelo <jperezlondono@gmail.com>
2013-12-16 12:30:11 -0500

    pdf

--- app/admin/sale.rb
+++ app/admin/sale.rb
@@ -81,20 +81,12 @@ form do |f|
 member_action :bill, :method => :get do
 @bill =  Sale.find(id = params[:id])
 
-# end
-# member_action :bill, :method => :get do
-# # @bill =  PaymentsCredit.find(id = params[:id])
-# @bill =  PaymentsCredit.find(14)
-# @credit = Credit.find(@bill.credit_id)
-# @mes = @bill.date.strftime("%B")
-# @credits = PaymentsCredit.select(:id, :date, :value, :interes).where(credit_id: @bill.credit_id).order("id ASC")
-# @payment_value = PaymentsCredit.select(:id, :date, :value, :interes).where(credit_id: @bill.credit_id).last
 
-# @nameUser = current_admin_user.name
-# vista creada en views/admin/credits/contract
+# vista creada en views/admin/credits/bill
      html = render_to_string(:action => "bill.html.erb", :layout => false)
       kit = PDFKit.new(html)
-      kit.stylesheets << 'vendor/assets/stylesheets/style_bill.css'
+      # kit.stylesheets << 'vendor/assets/stylesheets/style_bill.css'
+      kit.stylesheets << 'vendor/assets/stylesheets/style_bill2.css'
     send_data(kit.to_pdf, :filename => 'facturaPago.pdf', :type => 'application/pdf', :disposition => 'inline')
 end
 
