a326f4084d53352f02932cad8bbf282ccdd90d66
mrjusepelo <jperezlondono@gmail.com>
2014-01-06 15:55:10 -0500

    migraciones

--- db/migrate/20131226214611_add_interescolumns_to_credits.rb
+++
@@ -1,6 +0,0 @@
-class AddInterescolumnsToCredits < ActiveRecord::Migration
-  def change
-    add_column :credits, :interescorriente, :integer
-    add_column :credits, :interesmora, :integer
-  end
-end
