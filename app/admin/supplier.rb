668dcc0dcb4b4ae5fba5e2b57430d5f0f9b93266
mrjusepelo <jperezlondono@starenweb.com>
2014-02-11 22:57:03 -0500

    form suppliers

--- app/admin/supplier.rb
+++ app/admin/supplier.rb
@@ -31,41 +31,41 @@ actions :all, :except => [:destroy]
 
 
 form do |f|
-			f.inputs "Proveedores" do    show do |inventory|
-      attributes_table do 
-        row :id
-        row :product_id
-        # product = Product.where(id: inventory.product_id).first
-        # brand = Brand.where(id: product.brand_id).first
-        row "Marca" do
-          if defined?(inventory.product.brand.name)
-           marca = inventory.product.brand.name
-          else
-            marca = "Sin asignar"
+			f.inputs "Proveedores" do |inventory|
+        attributes_table do 
+          row :id
+          row :product_id
+          # product = Product.where(id: inventory.product_id).first
+          # brand = Brand.where(id: product.brand_id).first
+          row "Marca" do
+            if defined?(inventory.product.brand.name)
+             marca = inventory.product.brand.name
+            else
+              marca = "Sin asignar"
+            end
           end
+          row :supplier_id
+          row :barcode
+          row :serial
+          row :vale_buy
+          row :vale_sale
+          row :iva
+          row :warranty
+          row :date_in
+          row :date_out
+          row :created_at
+          row :updated_at
         end
-        row :supplier_id
-        row :barcode
-        row :serial
-        row :vale_buy
-        row :vale_sale
-        row :iva
-        row :warranty
-        row :date_in
-        row :date_out
-        row :created_at
-        row :updated_at
       end
-    end
-				f.input :name
-				f.input :phone
-				f.input :email
-				f.input :city
-			
-				#quitaado por q no se esta validando
-				# f.input :frecuence_notification
-				#quitaado por q no se esta validando
-				# f.input :state 
+  				f.input :name
+  				f.input :phone
+  				f.input :email
+  				f.input :city
+  			
+  				#quitaado por q no se esta validando
+  				# f.input :frecuence_notification
+  				#quitaado por q no se esta validando
+  				# f.input :state 
 
 			end
 			f.actions
