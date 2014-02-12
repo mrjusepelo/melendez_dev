578276419622db963d3e5ec497850e104d8ce1e0
mrjusepelo <jperezlondono@starenweb.com>
2014-02-11 23:06:40 -0500

    proveedores

--- app/admin/supplier.rb
+++ app/admin/supplier.rb
@@ -4,8 +4,6 @@ actions :all, :except => [:destroy]
  config.comments = true
 
   index do
-    # column :name
-    # row :nextdate
     column :id
     column :name 
     column :phone 
@@ -22,6 +20,7 @@ actions :all, :except => [:destroy]
         row :name
         row :phone
         row :email
+        row :city
         row :created_at
         row :updated_at
       end
@@ -30,33 +29,13 @@ actions :all, :except => [:destroy]
     end
 
 
+
+
+
 form do |f|
 			f.inputs "Proveedores" do |inventory|
-        attributes_table do 
-          row :id
-          row :product_id
-          # product = Product.where(id: inventory.product_id).first
-          # brand = Brand.where(id: product.brand_id).first
-          row "Marca" do
-            if defined?(inventory.product.brand.name)
-             marca = inventory.product.brand.name
-            else
-              marca = "Sin asignar"
-            end
-          end
-          row :supplier_id
-          row :barcode
-          row :serial
-          row :vale_buy
-          row :vale_sale
-          row :iva
-          row :warranty
-          row :date_in
-          row :date_out
-          row :created_at
-          row :updated_at
-        end
-      end
+
+      # end
   				f.input :name
   				f.input :phone
   				f.input :email
